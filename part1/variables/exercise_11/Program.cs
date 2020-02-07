c666be7cb16fbe3df29ee08d8c83cf96c4b00e4f
richard63Alan <60193348+richard63Alan@users.noreply.github.com>
2020-02-07 12:02:23 +0200

    ex11

--- part1/variables/exercise_11/Program.cs
+++ part1/variables/exercise_11/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 
 namespace exercise_11
 {
@@ -6,9 +7,9 @@ namespace exercise_11
     {
         public static void Main(string[] args)
         {
-            int dayCount = 100;
-            double hoursToLunch = 1;
-            string statement = "Are you sure?";
+            int dayCount = 200;
+            double hoursToLunch = 3.5;
+            string statement = "It sure is!";
 
             // DO NOT TOUCH THESE!
             Console.WriteLine("Days to summer:");
