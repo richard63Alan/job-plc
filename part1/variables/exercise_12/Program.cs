c9a7e941c1a09b04ca718fa30f8ed8896df456aa
richard63Alan <60193348+richard63Alan@users.noreply.github.com>
2020-02-07 14:05:50 +0200

    ex12

--- part1/variables/exercise_12/Program.cs
+++ part1/variables/exercise_12/Program.cs
@@ -7,7 +7,11 @@ namespace exercise_12
     public static void Main(string[] args)
     {
       // Write your code here:
+      Console.WriteLine ("Give a number!");
+      int intValue = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine("You gave " + intValue);
 
     }
   }
+
 }
